9ff915266d0dcc37c05eb207139396ca6c73004e
Radek Tesař <radek.tesar135@gmail.com>
2024-03-20 20:16:12 +0100

    Added basic tkinter structure

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import requests
 from tkinter import *
 
+# Main window
+root = Tk()
+root.minsize(300, 300)
+root.resizable(False, False)
+root.title("Aplikace na urážky")
+
+
 response = requests.get("https://evilinsult.com/generate_insult.php?lang=en&type=json")
 response.raise_for_status()
 data = response.json()
