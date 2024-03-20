93c51eea39bc396d7cf8ccc68529ebed9db771da
Radek Tesař <radek.tesar135@gmail.com>
2024-03-20 20:27:19 +0100

    Added roller for lang change

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ root.minsize(300, 300)
 root.resizable(False, False)
 root.title("Aplikace na urážky")
 
+# Roller shutter - language
+drop_down_lang = StringVar(root)
+drop_down_lang.set("cs")
+drop_down_lang_options = OptionMenu(root, drop_down_lang, "en", "cs", "pl", "es", "fr")
+drop_down_lang_options.pack()
 
 response = requests.get("https://evilinsult.com/generate_insult.php?lang=en&type=json")
 response.raise_for_status()
 data = response.json()
-
-print(data["insult"])
